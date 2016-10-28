5ebcdcf10c4832aa1f883d8cafd4ffabc12a2b3d
stephhappens <stephanie.n.provence@gmail.com>
2016-10-28 09:20:25 -0500

    start vue js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,34 @@
 import 'whatwg-fetch';
+import Vue from 'vue/dist/vue';
+
+const app = new Vue({
+  el: '.full-page',
+
+  data() {
+    return {
+      currentSelection: null,
+      seriesData: null,
+      series: null,
+    };
+  },
+
+  mounted() {
+    fetch('http://gateway.marvel.com:80/v1/public/series/9856%20?apikey=45b8eb3f93ddf64f6d341cdee0ee93d6')
+    .then((r) => r.json())
+      .then((data) => {
+        // this.series = data;
+        this.searchSeries('Hulk');
+      });
+  },
+
+  methods: {
+    searchSeries(search) {
+      fetch('http://gateway.marvel.com:80/v1/public/series/9856%20?apikey=45b8eb3f93ddf64f6d341cdee0ee93d6')
+
+      .then((r) => r.json())
+        .then((data) => {
+          this.seriesData = data;
+        });
+    },
+  },
+});
