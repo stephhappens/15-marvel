ffdc6eb697200ff0620907592f797b50d0603db4
stephhappens <stephanie.n.provence@gmail.com>
2016-10-28 11:34:44 -0500

    fix broken vue js

--- app/index.js
+++ app/index.js
@@ -1,30 +1,25 @@
 import 'whatwg-fetch';
 import Vue from 'vue/dist/vue';
 
+const apikey = '45b8eb3f93ddf64f6d341cdee0ee93d6';
+
+
 const app = new Vue({
   el: '.full-page',
 
   data() {
     return {
-      currentSelection: null,
       seriesData: null,
-      series: null,
     };
   },
 
   mounted() {
-    fetch('http://gateway.marvel.com:80/v1/public/series/9856%20?apikey=45b8eb3f93ddf64f6d341cdee0ee93d6')
-    .then((r) => r.json())
-      .then((data) => {
-        // this.series = data;
-        this.searchSeries('Hulk');
-      });
+    this.searchSeries('Hulk');
   },
 
   methods: {
-    searchSeries(search) {
-      fetch('http://gateway.marvel.com:80/v1/public/series/9856%20?apikey=45b8eb3f93ddf64f6d341cdee0ee93d6')
-
+    searchSeries(series) {
+      fetch(`http://gateway.marvel.com/v1/public/series?limit=1&titleStartsWith=${series}&apikey=${apikey}`)
       .then((r) => r.json())
         .then((data) => {
           this.seriesData = data;
